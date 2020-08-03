9b8d2fe1022dc2e26aec3d654c1fc393c2ebb4a6
kumquat-ir <66188216+kumquat-ir@users.noreply.github.com>
2020-08-03 16:43:21 -0700

    first actual generation

--- World.gd
+++ World.gd
@@ -26,7 +26,7 @@ extends Spatial
 #12:	choose side passage
 var genHooks:Array = [
 	[
-		[0, 0, 0, 0], [0]
+		[0, 0, 0, 1], [3]
 	]
 ]
 
@@ -41,15 +41,6 @@ func _ready():
 		rng.randomize()
 	else:
 		rng.set_seed(hash(setSeed))
-	for dir in range(4):
-		var dsl = dirSum2([5, 2, 0, 0], dir)
-		var dss = dirSum2([0, 4, 2, 0], dir)
-		print(prismArray(dsl[0], 3, dsl[1], dss[0], 0, dss[1]))
-		$tiles.setPrism(dsl[0], 3, dsl[1], dss[0], 0, dss[1], 2)
-	$"tile-metadata".setLine(AXIS_Y, 20, 0, -10, 0, 1)
-	$"tile-metadata".setLine(AXIS_X, 20, -10, 0, 0, 2)
-	$"tile-metadata".setLine(AXIS_Z, 20, 0, 0, -10, 3)
-	$"tile-metadata".setSingle(0, 0, 0, 0)
 
 func _process(_delta):
 	if Input.is_action_just_pressed("ui_accept"):
@@ -65,11 +56,11 @@ func roll(sides:int, omit:Array = []) -> int:
 func genStep():
 	if len(genHooks) == 0:
 		print("no hooks left to generate")
-		return
+		return 1
 	if len(genHooks[0]) != 2 or len(genHooks[0][0]) != 4 or len(genHooks[0][1]) < 1:
 		printerr("malformed hook " + str(genHooks[0]) + ", removing")
 		genHooks.remove(0)
-		return
+		return 0
 	var x = genHooks[0][0][0]
 	var y = genHooks[0][0][1]
 	var z = genHooks[0][0][2]
@@ -77,19 +68,19 @@ func genStep():
 	var dir = genHooks[0][1][0]
 	match symbol:
 		0:
-			print("generating start room at " + str(x) +  "/" + str(y) + "/" + str(z) + "-" + str(dir))
+			print("generating start room at " + str(x) +  "/" + str(y) + "/" + str(z) + "|" + str(dir))
 			genStart(x, y, z, dir)
 		1:
-			print("generating 5ft passage at " + str(x) +  "/" + str(y) + "/" + str(z) + "-" + str(dir))
+			print("generating 5ft passage at " + str(x) +  "/" + str(y) + "/" + str(z) + "|" + str(dir))
 			gen5pass(x, y, z, dir)
 		2:
-			print("generating 10ft passage at " + str(x) +  "/" + str(y) + "/" + str(z) + "-" + str(dir))
+			print("generating 10ft passage at " + str(x) +  "/" + str(y) + "/" + str(z) + "|" + str(dir))
 			gen10pass(x, y, z, dir)
 		3:
-			print("generating 20ft passage at " + str(x) +  "/" + str(y) + "/" + str(z) + "-" + str(dir))
+			print("generating 20ft passage at " + str(x) +  "/" + str(y) + "/" + str(z) + "|" + str(dir))
 			gen20pass(x, y, z, dir)
 		4:
-			print("generating 30ft passage at " + str(x) +  "/" + str(y) + "/" + str(z) + "-" + str(dir))
+			print("generating 30ft passage at " + str(x) +  "/" + str(y) + "/" + str(z) + "|" + str(dir))
 			gen30pass(x, y, z, dir)
 		5:
 			if len(genHooks[0][1] != 4):
@@ -99,39 +90,58 @@ func genStep():
 				var h = genHooks[0][1][2]
 				var gallery = genHooks[0][1][2]
 				print("generating 40ft passage (" + str(pillars) + "/" + str(h) + "/" + str(gallery) +
-						") at " + str(x) +  "/" + str(y) + "/" + str(z) + "-" + str(dir))
+						") at " + str(x) +  "/" + str(y) + "/" + str(z) + "|" + str(dir))
 				gen40pass(x, y, z, dir, pillars, h, gallery)
 		6:
-			print("generating door at " + str(x) +  "/" + str(y) + "/" + str(z) + "-" + str(dir))
+			print("generating door at " + str(x) +  "/" + str(y) + "/" + str(z) + "|" + str(dir))
 			genDoor(x, y, z, dir)
 		7:
-			print("generating beyond door at " + str(x) +  "/" + str(y) + "/" + str(z) + "-" + str(dir))
+			print("generating beyond door at " + str(x) +  "/" + str(y) + "/" + str(z) + "|" + str(dir))
 			genPastDoor(x, y, z, dir)
 		8:
-			print("generating chamber at " + str(x) +  "/" + str(y) + "/" + str(z) + "-" + str(dir))
+			print("generating chamber at " + str(x) +  "/" + str(y) + "/" + str(z) + "|" + str(dir))
 			genRoom(x, y, z, dir)
 		9:
-			print("generating chamber exit at " + str(x) +  "/" + str(y) + "/" + str(z) + "-" + str(dir))
+			print("generating chamber exit at " + str(x) +  "/" + str(y) + "/" + str(z) + "|" + str(dir))
 			genRoomExit(x, y, z, dir)
 		10:
-			print("generating stairs at " + str(x) +  "/" + str(y) + "/" + str(z) + "-" + str(dir))
+			print("generating stairs at " + str(x) +  "/" + str(y) + "/" + str(z) + "|" + str(dir))
 			genStairs(x, y, z, dir)
 		11:
-			print("choosing passage for " + str(x) +  "/" + str(y) + "/" + str(z) + "-" + str(dir))
+			print("choosing passage for " + str(x) +  "/" + str(y) + "/" + str(z) + "|" + str(dir))
 			choosePass(x, y, z, dir)
 		12:
-			print("choosing side passage for " + str(x) +  "/" + str(y) + "/" + str(z) + "-" + str(dir))
+			print("choosing side passage for " + str(x) +  "/" + str(y) + "/" + str(z) + "|" + str(dir))
 			chooseSidePass(x, y, z, dir)
 		_:
 			printerr("found unknown gen symbol " + str(symbol) + " at " + str(x) + 
-					"/" + str(y) + "/" + str(z) + "-" + str(dir) + ", removing")
+					"/" + str(y) + "/" + str(z) + "|" + str(dir) + ", removing")
 	genHooks.remove(0)
+	return 0
 
 func genStart(x:int, y:int, z:int, dir:int):
 	pass
 
 func gen5pass(x:int, y:int, z:int, dir:int):
-	pass
+	var bounds = dirSum2([3, 7, 0, 0], dir)
+	var origin = dirSum2([0, 0, 1, 0], dir)
+	var exclude = range(2, 21)
+	if not checkArray(prismArray(bounds[0], 3, bounds[1], origin[0] + x, y, origin[1] + z)):
+		exclude.append(1)
+	match roll(20, exclude):
+		1:
+			print("30ft straight, continue")
+			bounds = dirSum2([3, 7, 0, 0], dir)
+			origin = dirSum2([0, 0, 1, 0], dir)
+			$tiles.setPlane(PLANE_XZ, bounds[0], bounds[1], origin[0] + x, y - 1, origin[1] + z, 2)
+			bounds = dirSum2([0, 6, 0, 0], dir)
+			$tiles.setPrism(bounds[0], 2, bounds[1], origin[0] + x, y, origin[1] + z, 1)
+			origin = dirSum2([1, 0, 0, 0], dir)
+			$tiles.setPrism(bounds[0], 2, bounds[1], origin[0] + x, y, origin[1] + z, 1)
+			var disp = dirSum2([0, 6, 0, 0], dir)
+			genHooks.append([
+				[disp[0] + x, y, disp[1] + z, 1],
+			[dir]])
 
 func gen10pass(x:int, y:int, z:int, dir:int):
 	pass
