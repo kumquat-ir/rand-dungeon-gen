4ba3cfa6912fe8879eb975916b33c10733c3223f
kumquat-ir <66188216+kumquat-ir@users.noreply.github.com>
2020-08-03 23:57:04 -0700

    helpers and more gens

--- World.gd
+++ World.gd
@@ -18,6 +18,7 @@ extends Spatial
 #			- height: 10, 20
 #			- gallery: true, false
 #6: 	door
+#			- force: 0 - 5
 #7: 	beyond door
 #8: 	chamber
 #9: 	chamber exit
@@ -51,7 +52,9 @@ func roll(sides:int, omit:Array = []) -> int:
 	for i in omit:
 		if i - 1 in vals:
 			vals.remove(vals.find(i - 1))
-	return vals[rng.randi_range(0, len(vals) - 1)] + 1
+	return -1 if len(vals) < 1 else vals[rng.randi_range(0, len(vals) - 1)] + 1
+
+# ----- ACTUAL GENERATION -----
 
 func genStep():
 	if len(genHooks) == 0:
@@ -94,7 +97,12 @@ func genStep():
 				gen40pass(x, y, z, dir, pillars, h, gallery)
 		6:
 			print("generating door at " + str(x) +  "/" + str(y) + "/" + str(z) + "|" + str(dir))
-			genDoor(x, y, z, dir)
+			if len(genHooks[0][1]) == 2:
+				var force = genHooks[0][1][1]
+				print("forcing door type " + str(force))
+				genDoor(x, y, z, dir, force)
+			else:
+				genDoor(x, y, z, dir)
 		7:
 			print("generating beyond door at " + str(x) +  "/" + str(y) + "/" + str(z) + "|" + str(dir))
 			genPastDoor(x, y, z, dir)
@@ -123,25 +131,49 @@ func genStart(x:int, y:int, z:int, dir:int):
 	pass
 
 func gen5pass(x:int, y:int, z:int, dir:int):
-	var bounds = dirSum2([3, 7, 0, 0], dir)
+	var exclude = [2, 3, 4, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+	var bounds = dirSum2([3, 6, 0, 0], dir)
 	var origin = dirSum2([0, 0, 1, 0], dir)
-	var exclude = range(2, 21)
 	if not checkArray(prismArray(bounds[0], 3, bounds[1], origin[0] + x, y, origin[1] + z)):
 		exclude.append(1)
+		exclude.append(5)
 	match roll(20, exclude):
 		1:
 			print("30ft straight, continue")
-			bounds = dirSum2([3, 7, 0, 0], dir)
-			origin = dirSum2([0, 0, 1, 0], dir)
-			$tiles.setPlane(PLANE_XZ, bounds[0], bounds[1], origin[0] + x, y - 1, origin[1] + z, 2)
-			bounds = dirSum2([0, 6, 0, 0], dir)
-			$tiles.setPrism(bounds[0], 2, bounds[1], origin[0] + x, y, origin[1] + z, 1)
-			origin = dirSum2([1, 0, 0, 0], dir)
-			$tiles.setPrism(bounds[0], 2, bounds[1], origin[0] + x, y, origin[1] + z, 1)
-			var disp = dirSum2([0, 6, 0, 0], dir)
-			genHooks.append([
-				[disp[0] + x, y, disp[1] + z, 1],
-			[dir]])
+#			bounds = dirSum2([3, 7, 0, 0], dir)
+#			origin = dirSum2([0, 0, 1, 0], dir)
+#			$tiles.setPlane(PLANE_XZ, bounds[0], bounds[1], origin[0] + x, y - 1, origin[1] + z, 2)
+#			bounds = dirSum2([0, 6, 0, 0], dir)
+#			$tiles.setPrism(bounds[0], 2, bounds[1], origin[0] + x, y, origin[1] + z, 1)
+#			origin = dirSum2([1, 0, 0, 0], dir)
+#			$tiles.setPrism(bounds[0], 2, bounds[1], origin[0] + x, y, origin[1] + z, 1)
+#			bounds = dirSum2([5, 6, 0, 0], dir)
+#			origin = dirSum2([0, 0, 2, 0], dir)
+#			$"tile-metadata".setPrism(bounds[0], 2, bounds[1], origin[0] + x, y, origin[1] + z, 1)
+			passageBase([3, 7, 0, 0], x, y, z, dir)
+#			var disp = dirSum2([0, 6, 0, 0], dir)
+#			genHooks.append([
+#				[disp[0] + x, y, disp[1] + z, 1],
+#			[dir]])
+			newHooks([[0, 6, 0, 0]], [1], x, y, z, dir)
+		5:
+			print("20ft straight to door")
+			passageBase([3, 5, 0, 0], x, y, z, dir)
+			newHooks([[0, 4, 0, 0]], [6], x, y, z, dir)
+		10:
+			print("20ft straight to possible secret door")
+			passageBase([3, 5, 0, 0], x, y, z, dir)
+			if roll(10) == 10:
+				newHooks([[0, 4, 0, 0]], [6], x, y, z, dir, [[5]])
+			else:
+				bounds = dirSum2([3, 0, 0, 0], dir)
+				origin = dirSum2([0, 4, 1, 0], dir)
+				$tiles.setPrism(bounds[0], 2, bounds[1], origin[0] + x, y, origin[1] + z, 1)
+				bounds = dirSum2([5, 2, 0, 0], dir)
+				origin = dirSum2([0, 4, 2, 0], dir)
+				$"tile-metadata".setPrism(bounds[0], 2, bounds[1], origin[0] + x, y, origin[1] + z, 1)
+		_:
+			print("no space for anything")
 
 func gen10pass(x:int, y:int, z:int, dir:int):
 	pass
@@ -155,7 +187,15 @@ func gen30pass(x:int, y:int, z:int, dir:int):
 func gen40pass(x:int, y:int, z:int, dir:int, pillar:int, h:int, gallery:bool):
 	pass
 
-func genDoor(x:int, y:int, z:int, dir:int):
+#forces:
+#	0: none
+#	1: wooden
+#	2: stone
+#	3: iron
+#	4: portcullis
+#	5: secret
+#will probably only use secret, including others for completion
+func genDoor(x:int, y:int, z:int, dir:int, force:int = 0):
 	pass
 
 func genPastDoor(x:int, y:int, z:int, dir:int):
@@ -176,6 +216,30 @@ func choosePass(x:int, y:int, z:int, dir:int):
 func chooseSidePass(x:int, y:int, z:int, dir:int):
 	pass
 
+# ----- GENERATION HELPERS -----
+
+func passageBase(fbounds:Array, x:int, y:int, z:int, dir:int, h:int = 2):
+	var bounds = dirSum2(fbounds, dir)
+	var origin = dirSum2([0, 0, 1, 0], dir)
+	$tiles.setPlane(PLANE_XZ, bounds[0], bounds[1], origin[0] + x, y - 1, origin[1] + z, 2)
+	bounds = dirSum2([0, fbounds[1] - 1, 0, 0], dir)
+	$tiles.setPrism(bounds[0], 2, bounds[1], origin[0] + x, y, origin[1] + z, 1)
+	origin = dirSum2([fbounds[0] - 2, 0, 0, 0], dir)
+	$tiles.setPrism(bounds[0], 2, bounds[1], origin[0] + x, y, origin[1] + z, 1)
+	bounds = dirSum2([fbounds[0] + 2, fbounds[1] - 1, 0, 0], dir)
+	origin = dirSum2([0, 0, 2, 0], dir)
+	$"tile-metadata".setPrism(bounds[0], 2, bounds[1], origin[0] + x, y, origin[1] + z, 1)
+
+func newHooks(disps:Array, vals:Array, x:int, y:int, z:int, dir:int, extras:Array = [[]]):
+	for i in len(disps):
+		var disp = dirSum2(disps[i], dir)
+		var arr2 = [dir]
+		for extra in extras[i]:
+			arr2.append(extra)
+		genHooks.append([
+			[disp[0] + x, y, disp[1] + z, vals[i]],
+		arr2])
+
 #checks array of x/y/z coords to see if the space is open (tile-metadata is 0 or nonexistant)
 func checkArray(arr:Array) -> bool:
 	for coords in arr:
@@ -191,6 +255,8 @@ func prismArray(lx:int, ly:int, lz:int, sx:int, sy:int, sz:int) -> Array:
 			for z in range(min(lz + 1, 0), max(lz, 1)):
 				arr.append([x + sx, y + sy, z + sz])
 	return arr
+
+# ----- DIRECTION HANDLING -----
 
 func forward(amt:int, dir:int) -> Array:
 	match dir:
@@ -240,6 +306,7 @@ func back(amt:int, dir:int) -> Array:
 			return [0, amt]
 	return [0, 0]
 
+#probably won't end up using this one, less flexible
 func dirSum1(amts:Array, xz: int, dir:int) -> int:
 	return right(amts[0], dir)[xz] + forward(amts[1], dir)[xz] + left(amts[2], dir)[xz] + back(amts[3], dir)[xz]
 
